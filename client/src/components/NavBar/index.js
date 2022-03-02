d752ee9aebfc7c9181b8bce90cab2b42e7d35c50
Nick M <mully7773@gmail.com>
2022-03-02 17:33:52 -0600

    more styling

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -5,25 +5,29 @@ import "./style.css"
 
 
 const NavBar = () => {
-  
-  
-  
-  return ( 
-<div>
-<Navbar className='nav-main' bg="" variant="light">
-    <Container>
-    <Nav className="nav me-auto">
-      <Nav.Link className="nav-item" as={Link} to="/">Home</Nav.Link>
-      <Nav.Link className="nav-item"as={Link} to="/login">Login</Nav.Link>
-      <Nav.Link className="nav-item" as={Link} to="/signup">Sign Up</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  </div>
+    return (
+        <div>
+            <Navbar className='nav-main' bg="" variant="light">
+                <Container>
+                    <Navbar.Brand className="nav-brand" href="#home">
+                        <img className="emblem"
+                            src={`${process.env.PUBLIC_URL}/assets/images/icons8-dungeons-and-dragons-256.png`}
+                        />
+                        <span className='capitalD'>D</span>ungeon <span className='capitalD'>D</span>elver</Navbar.Brand>
+                </Container>
+                <Container>
+                    <Nav className="nav me-auto">
+                        <Nav.Link className="nav-item" as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link className="nav-item" as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link className="nav-item" as={Link} to="/signup">Sign Up</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
 
 
-       
+
     )
 };
- 
+
 export default NavBar;
