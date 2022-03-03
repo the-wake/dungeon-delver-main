773a907005deefaa3d6ad2962afce1453b9d280b
Nick M <mully7773@gmail.com>
2022-03-02 18:53:55 -0600

    Added a few pages and components - boilerplate code

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //components:
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+//pages:
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
     <Router>
     <div className="App">
       <NavBar />
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="login" element={<Login/>} />
+        <Route path="signup" element={<SignUp/>} />
+      </Routes>
+     <Footer />
     </div>
     </Router>
   );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="text-center">
+            <p>Copyright © 2022 | DungeonDelver</p>
+        </footer>
+    );
+}
+
+export default Footer;
