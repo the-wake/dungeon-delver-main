ff6bc42df58eceb007ceee839bcd7d0c4a514b33
Nick M <mully7773@gmail.com>
2022-03-06 15:31:21 -0600

    added to the dungeonList component

--- client/src/components/DungeonList/index.js
+++ client/src/components/DungeonList/index.js
@@ -1,7 +1,62 @@
-const DungeonList = () => {
-    return ( 
-        <div></div>
-     );
+import { Card, Container, Row, Col, Button } from 'react-bootstrap';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { REMOVE_DUNGEON } from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
+import "./dungeonList.css";
+
+const DungeonList = ({ dungeons }) => {
+    const [removeDungeon, { error }] = useMutation(REMOVE_DUNGEON, {
+        update(cache, { data: { removeDungeon } }) {
+            try {
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: removeDungeon },
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    });
+
+    const handleRemoveDungeon = async (_id) => {
+        try {
+            const { data } = await removeDungeon({
+                variables: { _id },
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    if (!dungeons.length) {
+        return <h3>You have no dungeons yet...</h3>
+    }
+
+    return (
+        <Container>
+
+            <Row xs={1} md={2} lg={3} className="g-4">
+                {dungeons && dungeons.map((dungeon) => (
+                    <Col>
+                        <Card>
+                            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                            <Card.Body>
+                                <Card.Title key={dungeon._id} className="dungeon-title">{dungeon.name}</Card.Title>
+                                <Card.Text>
+                                    We can add a field for dungeon description here. Need to add another field to ADD_DUNGEON.
+                                </Card.Text>
+                                {Auth.loggedIn && (<Button
+                                    onClick={() => handleRemoveDungeon(dungeon)}
+                                >X</Button>)}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
- 
+
 export default DungeonList;
