08bb2f6a79ee18bb31a1c4f8f2283d7633d56ea4
Ben Martin <bmartin2009@gmail.com>
2022-10-03 22:40:41 -0500

    Trying some fixes for Heroku.

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 };
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+ };
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
